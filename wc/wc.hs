142c060f5a7d7be57afa58c9d95b3b0af4500520
Mark van den Bosch <markvandenbosch@gmail.com>
2023-12-18 11:09:05 +0100

    Fixed small comment typo's

--- wc/wc.hs
+++ wc/wc.hs
@@ -25,30 +25,30 @@ main = do
 
 handleFileInput :: (B.ByteString -> String) -> String -> IO ()
 handleFileInput option file =
-    B.readFile file >>= \content -> 
+    B.readFile file >>= \content ->
         putStrLn $ "  " ++ option content ++ " " ++ file
 
 handleStdInput :: (B.ByteString -> String) -> IO ()
 handleStdInput option = B.getContents >>=
     \content -> putStrLn $ "  " ++ option content
 
--- |Get the number of bytes from a string
+-- |Get the number of bytes from a ByteString
 getBytes :: B.ByteString -> String
 getBytes = show . B.length
 
--- |Get the number of lines from a string
+-- |Get the number of lines from a ByteString
 getLines :: B.ByteString -> String
 getLines = show . length . C.lines
 
--- |Get the number of words from a string
+-- |Get the number of words from a ByteString
 getWords :: B.ByteString -> String
 getWords = show . length . C.words
 
--- |Get the number of characters from a string
+-- |Get the number of characters from a ByteString
 getChars :: B.ByteString -> String
 getChars = show . T.length . E.decodeUtf8
 
--- |Get the default option which is bytes, lines, and words
+-- |Get the default option which is lines, words, and bytes
 defaultOption :: B.ByteString -> String
 defaultOption content =
     getLines content ++ "  " ++  getWords content ++ "  " ++ getBytes content
